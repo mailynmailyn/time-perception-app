f3b16ce17646948b2d74be718832511ba9443162
Mai Lyn Puittinen <60805786+mailynmailyn@users.noreply.github.com>
2024-11-05 23:19:27 -0500

    set home page to audioplayer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AudioPlayer from './components/AudioPlayer'; // Adjust the path if necessary
 import Instructions from './components/Instructions';
-import UserInfo from './components/UserInfo';
+// import UserInfo from './components/UserInfo';
 import SemanticData from './components/SemanticData';
 import Results from './components/Results';
 
@@ -15,9 +15,9 @@ function App() {
       </nav> */}
 
       <Routes>
-        <Route path="/" element={<UserInfo />} />
+        <Route path="" element={<AudioPlayer />} />
         <Route path="/instructions" element={<Instructions />} />
-        <Route path="/audioplayer" element={<AudioPlayer />} />
+        // <Route path="/audioplayer" element={<AudioPlayer />} />
         <Route path="/semanticdata" element={<SemanticData />} />
         <Route path="/restils" element={<Results />} />
       </Routes>
